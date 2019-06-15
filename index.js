e70d2968f87b81f4454a7c97cd572c04ebdbc744
Varis Darasirikul <varis.dara@gmail.com>
2019-06-15 17:16:06 +0800

    finish add basic client structure

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { version } from './helper/variables';
+
+const app = express()
+
+app.use(`${version}/api/**`, bodyParser.json())
+
+if (process.env.NODE_ENV === 'production') {
+  // Express wil serve up production assets
+  // like main.js, main.css
+  app.use(express.static('client/dist'))
+
+  // Express will serve up the index.hml file
+  // if does not recognize the route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 5000
+app.timeout = 300000
+const server = app.listen(PORT)
+
+export default server
