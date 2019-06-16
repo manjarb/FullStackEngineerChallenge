9f0a88ac438639dc2894929aa41716e6d33f43de
Varis Darasirikul <varis.dara@gmail.com>
2019-06-16 20:46:18 +0800

    fix placeholder text

--- client/src/container/AdminReview/AdminReview.js
+++ client/src/container/AdminReview/AdminReview.js
@@ -11,7 +11,7 @@ export class AdminReview extends React.Component {
     return (
       <Container>
         <AdminSidebar />
-        Admin Review Page euuueo eoeoueouuoe uuoeu eoae e e eeou
+        Admin Review Page
       </Container>
     )
   }
